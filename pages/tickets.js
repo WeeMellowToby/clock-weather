a082f4dcb33a22b3e48e37b1a6be58613225dc27
WeeMellowToby <69983951+WeeMellowToby@users.noreply.github.com>
2022-07-29 09:05:45 +0100

    added the ticket counter

--- pages/tickets.js
+++ pages/tickets.js
@@ -1,20 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from "next/router"
 import  Login  from "./components/login"
 import { getTickets } from '../tickets/ticketCount'
+import Clock from 'react-live-clock';
+import { GetWeatherInBrighton } from './api/weather';
+import Image from 'next/image';
 export default function Tickets({hasReadPermission}) {
+  const [tickets,SetTickets] = React.useState(0);
     useEffect(() => {
         if (hasReadPermission) {
-            getTickets(console.log)
+            getTickets(SetTickets);
         }
     }, [])
     const router = useRouter()
     if (!hasReadPermission) {
         return <Login redirectPath={router.asPath} />
       }
-  return (
-    <div >
-        
-    </div>
-  )
+      const [weather,setWeather] = useState(null);
+      useEffect(() => {
+        var timer = setInterval(function(){
+          GetWeatherInBrighton(setWeather);
+        }
+        ,60000);
+        GetWeatherInBrighton(setWeather);
+      }, [])
+    
+      function Capitalise(string) {
+        const words = string.split(" ");
+        for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+      }
+      return(words.join(" "));
+      }
+      return (
+        <div className=''>
+          <Image src="/images/background.jpg" layout="fill" className="opacity-25"/>
+          <div className = 'clocktickets'>
+          <Clock
+              format={'HH:mm:ss'}
+              ticking={true}
+              
+              />
+              {weather != null ? <p>{Math.round(weather.main.temp * 10) / 10}&#8451; <br/> {Capitalise(weather.weather[0].description)} <br/> Wind: {weather.wind.speed} km/h <br/> {tickets} Tickets Scanned </p> : ' Loading'}
+              
+          </div>
+    
+          <div className=' absolute bottom-10 right-10'>
+              <Image src="/images/logo.png" alt="logo" width={270} height={60} />
+          </div>
+    
+          </div>
+      )
 }
--- tickets/ticketCount.js
+++ tickets/ticketCount.js
@@ -5,6 +5,13 @@ export async function getTickets(_callback) {
           'authorization': 'Bearer '  + process.env.NEXT_PUBLIC_SCANNING_KEY, 
         }),
       });
-    let data = await response;
-    _callback(data);
+    let data = await (await response).json();
+    let tickets = 0;
+    // run through all the scanners in the data and add up the scanned tickets
+    for (let scanner in data.totals) {
+        tickets += data.totals[scanner].scanned;
+        
+        
+    }
+    _callback(tickets);
 }
