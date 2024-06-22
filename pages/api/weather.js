4366f617b7413490af8890278683a1448ad30612
WeeMellowToby <69983951+WeeMellowToby@users.noreply.github.com>
2024-06-22 08:53:37 +0100

    fixed merge conflicts with weather js

--- pages/api/weather.js
+++ pages/api/weather.js
@@ -1,6 +1,3 @@
-
-
-
 const key = `${process.env.NEXT_PUBLIC_WEATHER_KEY}`
 export async function GetWeatherHere(_callback) {
     if (navigator.geolocation) {
@@ -33,9 +30,7 @@ async function getWeather(url) {
 export async function wunderground(apiKey,stationID, callback) {
       let url= `https://api.weather.com/v2/pws/observations/current?stationId=${stationID}&format=json&units=m&apiKey=${apiKey}`
       let response = await fetch(url)
-      //let data = await response.json()
-      //console.log(data)
-      
+      let data = await response.json()
       callback(data.observations[0])
 
 }
