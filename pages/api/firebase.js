9991415db2ed3f12aa04aabb296bbd654645c94d
WeeMellowToby <69983951+WeeMellowToby@users.noreply.github.com>
2024-06-27 10:51:42 +0000

    Added AddEvent function

--- pages/api/firebase.js
+++ pages/api/firebase.js
@@ -1,6 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { collection, doc, getDocs, getFirestore,getDoc } from "firebase/firestore";
+import {
+    collection,
+    doc,
+    getDocs,
+    getFirestore,
+    getDoc,
+    addDoc,
+    GeoPoint,
+} from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
@@ -10,23 +18,29 @@ const firebaseConfig = {
     storageBucket: "clock-weather.appspot.com",
     messagingSenderId: "967194027131",
     appId: "1:967194027131:web:78c786d6a10686af0feede",
-    measurementId: "G-0ZN8MX5F8G"
+    measurementId: "G-0ZN8MX5F8G",
 };
 
-
-export async function getEvent(eventID,callback) {
-    console.log("Reading from DB")
+export async function getEvent(eventID, callback) {
+    console.log("Reading from DB");
     let docRef = doc(db, "events", eventID);
     let docSnap = await getDoc(docRef);
-    callback(docSnap.data())
-    console.log("DATA READ from DB")
+    callback(docSnap.data());
+    console.log("DATA READ from DB");
 }
 export async function getEvents(callback) {
     var events = await getDocs(collection(db, "events"));
-    if(callback) callback(events);
-
+    if (callback) callback(events);
 }
-
+export async function AddEvent(StartDate, EndDate, lat, lon, wunderground) {
+    let location = GeoPoint(lat, lon);
+    let docRef = await addDoc(collection(db, "events"), {
+        StartDate: StartDate,
+        EndDate: EndDate,
+        location: location,
+        wunderground: wunderground,
+    });
+}
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
