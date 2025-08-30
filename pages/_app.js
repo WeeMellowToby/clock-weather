555981b22dc9daa965697f69d969fcb8a8995839
Toby Hampton <tobyhampton@outlook.com>
2025-08-30 09:47:47 +0100

    updated imports

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import Cookies from "universal-cookie"
 import consts from "../consts"
 import App from "next/app"
-import { UserContext, useUserData } from './lib/context'
+import { UserContext, useUserData } from '../lib/context'
 function MyApp({ Component, pageProps }) {
   const userData = useUserData();
   return <UserContext.Provider value={userData}><Component {...pageProps} /></UserContext.Provider>
